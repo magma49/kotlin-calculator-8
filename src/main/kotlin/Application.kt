997c7968480744a752edd5177c124725862b8074
ndwPC <magma0409@gmail.com>
2025-11-18 19:41:54 +0900

    feat: 입력값에 따라 경우 분리하기

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,7 +4,16 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
-    val input = Console.readLine()
+    val input = Console.readLine().trim()
 
-    println(input)
+    var output = 0
+
+    if (input.isEmpty()) {
+        output = 0
+    } else if (input.take(2) == "//") {
+        output = 1
+    } else {
+        output = 2
+    }
+    println("결과: " + output)
 }
