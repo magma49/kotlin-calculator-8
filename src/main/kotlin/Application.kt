db0eb6c9d7ebf20a85f01b2b0409c42be60ab709
ndwPC <magma0409@gmail.com>
2025-11-24 20:20:25 +0900

    style: 더 명확하고 간결하게 수정

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,42 +4,40 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("덧셈할 문자열을 입력해 주세요.")
-    val input = Console.readLine().trim()
+    val input: String = Console.readLine().trim()
 
     var output = 0
 
-    var numString = emptyList<String>()
+    var numString: List<String> = emptyList()
 
     if (input.isEmpty()) {
         output = 0
     } else if (input.take(2) == "//") {
-        val inputSub = input.substring(2).split("\\n")
-        val syntax = "[,:" + inputSub[0] + "]"
+        val inputSub: List<String> = input.substring(2).split("\\n")
+        val syntax: String = "[,:" + inputSub[0] + "]"
         numString = inputSub[1].split(syntax.toRegex())
     } else {
         numString = input.split("[,:]".toRegex())
     }
     if (!numString.isEmpty())
-        output = changeInt(numString).sum()
+        output = sumNum(numString)
 
-    println("결과: " + output)
+    println("결과: $output")
 }
 
-fun changeInt(numString: List<String>): MutableList<Int> {
-    val numInt = mutableListOf<Int>()
+fun sumNum(numString: List<String>): Int {
+    var sum = 0
     for (numStr in numString) {
         var num: Int
-
         val numS = numStr.trim()
         if (numS == "") continue
         try {
             num = numS.toInt()
         } catch (e: NumberFormatException) {
-            throw IllegalArgumentException("[ERROR] 잘못된 입력: 구문 오류 또는 숫자 오류")
+            throw IllegalArgumentException("[ERROR] 숫자만 입력해주세요.")
         }
-        if (num < 0)
-            throw IllegalArgumentException("[ERROR] 잘못된 입력: 0이상인 숫자만 가능합니다")
-        numInt.add(num)
+        require(num >= 0) { "[ERROR] 0이상의 정수만 입력해주세요." }
+        sum += num
     }
-    return numInt
+    return sum
 }
